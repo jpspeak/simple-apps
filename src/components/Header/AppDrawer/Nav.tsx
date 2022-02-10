1fc836f4aa77ed0ff103412228133ad7df1307e6
Jay Philip <jayphilipcamillo@gmail.com>
2022-02-10 14:16:45 +0800

    add sum calculator in mobile nav

--- src/components/Header/AppDrawer/Nav.tsx
+++ src/components/Header/AppDrawer/Nav.tsx
@@ -8,6 +8,7 @@ const Nav = () => {
         <VStack spacing={12} alignItems='normal' fontWeight='semibold'>
           <NavLink to='/'>Hello World</NavLink>
           <NavLink to='/counter'>Counter</NavLink>
+          <NavLink to='/sum-calculator'>Sum Calculator</NavLink>
           <NavLink to='/stop-button'>Stop Button</NavLink>
           <NavLink to='/todo'>Todo</NavLink>
         </VStack>
